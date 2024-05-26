0530b1994eccb69a7966a2abb974d5f1e8e55bb9
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-05-26 15:48:05 +0200

    Fix aff param

--- starters/test.sh
+++ starters/test.sh
@@ -3,12 +3,16 @@
 cd `dirname $0`
 cp ../yasl_linux .
 
-MAPS=$(ls | grep .rgb.b64)
-for i in $MAPS
-do
-	echo "MAP: $i"
-	cat $i | ./display_b64
-	echo
-done
-
+echo Exercise 0 : yasl_hw && echo -------------------
+echo './yasl_hw | cat -e' && ./yasl_hw | cat -e
+echo && echo -------------------
+echo ./yasl_hw arg | cat -e && ./yasl_hw arg | cat -e
+echo && echo -------------------
+echo
+echo
+echo Exercise 1 : yasl_aff_param && echo -------------------
+echo ./yasl_aff_param && ./yasl_aff_param
+echo && echo -------------------
+echo ./yasl_aff_param 1 2 3 4 && ./yasl_aff_param 1 2 3 4
+echo && echo -------------------
 rm yasl_linux
